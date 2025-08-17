16e1a16edd89a5da8ebc0049ff282116eb74d567
ahallemberg <ahallemberg@gmail.com>
2025-08-17 18:04:52 +0200

    Created PUT endpoint to update r2 bucket

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,43 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+export interface Env {
+    MY_BUCKET: R2Bucket;
+    AUTH_KEY_SECRET: string;
+}
+
+const hasValidHeader = (request: Request, env: Env): boolean => {
+    return request.headers.get("X-Custom-API-Key") === env.AUTH_KEY_SECRET;
+};
+
+function authorizeRequest(request: Request, env: Env, key: string): boolean {
+    switch (request.method) {
+        case "PUT":
+            return hasValidHeader(request, env);
+        default:
+            return false;
+    }
+}
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+    async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+        const url = new URL(request.url);
+        const key = url.pathname.slice(1);
+        
+        if (!authorizeRequest(request, env, key)) {
+            return new Response("Forbidden", { status: 403 });
+        }
+        
+        switch (request.method) {
+            case "PUT":
+                await env.MY_BUCKET.put(key, request.body);
+                return new Response(`Put ${key} successfully!`);
+
+            default:
+                return new Response(`Method Not Allowed `, {
+                    status: 405,
+                    headers: {
+                        Allow: "PUT, GET, DELETE",
+                    },
+                    
+                });
+        }
+    },
+};
